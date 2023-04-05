da22a25cd41c345cf5afb7215811f143627da092
Ayo Moses <ayomoses@afrimash.com>
2023-04-05 16:41:11 +0100

    created animals table

--- queries.sql
+++ queries.sql
@@ -1,3 +1,8 @@
-/*Queries that provide answers to the questions from all projects.*/
-
-SELECT * from animals WHERE name = 'Luna';
+SELECT * from animals where name like '%mon%'
+SELECT * from animals where date_of_birth between '2016-01-01' and '2019-12-31'
+SELECT name from animals where neutered is true and escape_attempts < 3
+SELECT date_of_birth from animals where name = 'Agumon' or name = 'Pikachu'
+SELECT name, escape_attempts from animals where weight_kg > 10.5
+SELECT * from animals where neutered is true
+SELECT * from animals where name != 'Gabumon'
+SELECT * from animals where weight_kg >= 10.4 and weight_kg <= 17.3
