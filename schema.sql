94358dc08379e65b6ce3def69900f28b7e2c5acd
Ayo Moses <ayomoses@afrimash.com>
2023-04-07 16:29:27 +0100

    added new tables(owners and species) and modified the animals table

--- schema.sql
+++ schema.sql
@@ -11,3 +11,20 @@ CREATE TABLE animals(
 );
 
 ALTER TABLE animals ADD COLUMN species VARCHAR(255);
+
+CREATE TABLE owners(
+  id                INT GENERATED ALWAYS AS IDENTITY,
+  full_name         VARCHAR(100),
+  age               INT,
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE species(
+  id                INT GENERATED ALWAYS AS IDENTITY,
+  name              VARCHAR(100),
+  PRIMARY KEY(id)
+);
+
+ALTER TABLE animals DROP column species;
+ALTER TABLE animals ADD COLUMN species_id INT, ADD CONSTRAINT fk_species_id FOREIGN KEY (species_id) REFERENCES species(id);
+ALTER TABLE animals ADD COLUMN owners_id INT, ADD CONSTRAINT fk_owners_id FOREIGN KEY (owners_id) REFERENCES owners(id)
