5bd37513dd757da8500a733dc89d705ca3c79f48
Ayo Moses <ayomoses@afrimash.com>
2023-04-08 06:07:11 +0100

    integrated many-to-many relationships between tables

--- schema.sql
+++ schema.sql
@@ -28,3 +28,15 @@ CREATE TABLE species(
 ALTER TABLE animals DROP column species;
 ALTER TABLE animals ADD COLUMN species_id INT, ADD CONSTRAINT fk_species_id FOREIGN KEY (species_id) REFERENCES species(id);
 ALTER TABLE animals ADD COLUMN owners_id INT, ADD CONSTRAINT fk_owners_id FOREIGN KEY (owners_id) REFERENCES owners(id)
+
+CREATE TABLE vets(
+  id                	INT GENERATED ALWAYS AS IDENTITY,
+  name              	VARCHAR(100),
+  age               	INT,
+  date_of_graduation 	date,
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE specializations (  vet_id INTEGER REFERENCES vets(id),  species_id INTEGER REFERENCES species(id),  PRIMARY KEY (vet_id, species_id) );
+CREATE TABLE visits ( id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY, animal_id INT REFERENCES animals(id),	vet_id INT REFERENCES vets(id),	date_of_visit DATE );
+
